e20fe5bcd3765c5297f156f6ce4e269ff0902984
Andrii Kolchev <andrii.kolchev@pamind.se>
2024-07-01 13:25:13 +0200

    v1.0

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
